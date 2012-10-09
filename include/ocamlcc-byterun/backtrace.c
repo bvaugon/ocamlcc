7e24b12a0489f2a3db438679b0b34a9bd07cd40f
Benoît Vaugon <benoit.vaugon@gmail.com>
2012-10-09 20:16:00 +0200

    Fix gcc warning: "use of uninitialised struct".

--- include/ocamlcc-byterun/backtrace.c
+++ include/ocamlcc-byterun/backtrace.c
@@ -260,7 +260,7 @@ CAMLexport void caml_print_exception_backtrace(void)
 {
   value events;
   int i;
-  struct loc_info li;
+  struct loc_info li = {0};
 
   events = read_debug_info();
   if (events == Val_false) {
